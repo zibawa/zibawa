965c2077603b70c66ccdb3d9a4b1fb28ca9aa979
matt <matt.field@zibawa.com>
2017-07-12 11:23:34 +0200

    removed IoT_pki

--- IoT_pki/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class IotPkiConfig(AppConfig):
-    name = 'IoT_pki'
--- zibawa/urls.py
+++ zibawa/urls.py
@@ -17,21 +17,17 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 from django.conf.urls import url, include
-from rest_framework import routers
-from rest_framework.documentation import include_docs_urls
-from IoT_pki import views
 
-router = routers.DefaultRouter()
+
+
+
 
 #router.register(r'cert_request', views.cert_request)
 #router.register(r'get_approved_cert',views.get_approved_cert)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
-urlpatterns = [
-    url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-]
+
 
 
 urlpatterns = [
@@ -39,11 +35,8 @@ urlpatterns = [
     url(r'^devices/', include('devices.urls',namespace='devices')),
     url(r'^front/', include('front.urls',namespace='front')),
     url(r'^admin/', admin.site.urls),
-    url(r'^IoT_pki/', include('IoT_pki.urls',namespace='IoT_pki')),
     url(r'^', include('front.urls')),
-    url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^docs/', include_docs_urls(title='My API title'))
+    
     
     
     
