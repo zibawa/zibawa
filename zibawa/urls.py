0cd9e7a9a34402950a4bfe2c792b637fddb1c7e1
matt <matt.field@zibawa.com>
2017-07-21 13:21:56 +0200

    add pki

--- zibawa/urls.py
+++ zibawa/urls.py
@@ -13,21 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
+from django.conf.urls import url
 from django.contrib import admin
 
-from django.conf.urls import url, include
 
+from django.conf.urls import include
+from rest_framework import routers
+from rest_framework.documentation import include_docs_urls
+from IoT_pki import views
 
-
-
-
-#router.register(r'cert_request', views.cert_request)
-#router.register(r'get_approved_cert',views.get_approved_cert)
-
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
-
+router = routers.DefaultRouter()
 
 
 urlpatterns = [
@@ -36,6 +31,10 @@ urlpatterns = [
     url(r'^front/', include('front.urls',namespace='front')),
     url(r'^admin/', admin.site.urls),
     url(r'^', include('front.urls')),
+    url(r'^IoT_pki/', include('IoT_pki.urls',namespace='IoT_pki')),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^docs/', include_docs_urls(title='zibawa_PKI'))
     
     
     
