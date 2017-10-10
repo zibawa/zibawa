8ded7a2bf5355c85e5f76c7fb0a8a602e42d4b95
matt <matt.field@zibawa.com>
2017-10-10 15:31:20 +0200

    added api v1

--- devices/urls.py
+++ devices/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^testMessage/$', views.testMessage, name='test_message'),
+    url(r'^api_v1/$', views.testMessage, name='api_v1'),
     url(r'^(?P<device_id>[0-9]+)/resetPsw/$', views.resetPsw, name='resetPsw'),
     url(r'^download_CA_cert/$', views.download_CA_cert, name='download_CA_cert'),
     
